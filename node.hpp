4f2121faa049172dc1eac26e1415a154cb04d6b9
jacob-wilkens <jacob.wilkens@cuw.edu>
2020-02-14 14:23:10 -0600

    New Methods

--- node.hpp
+++ node.hpp
@@ -1,11 +1,14 @@
 #ifndef node_hpp
 #define node_hpp
+
 class node {
     private:
         int payload;
         node* nextNode;
     public:
         node(int payload);
+        int getPayload();
+        node* getNextNode();
         void setNextNode(node* newNode);
 
 };
